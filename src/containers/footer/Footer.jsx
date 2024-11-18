1deb14dd52d54e59afe2f2c45586b88a799951df
CyndieKamau <cyndiekamaa@gmail.com>
2024-11-18 15:34:32 +0700

    feat: Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css'
+import logo2 from '../../assets/logo2.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="pacx__footer section__padding">
+    <div className="pacx__footer-heading">
+      <h1 className="gradient__text">Book a Call with us to step into the future of Blockchain Learning</h1>
+    </div>
+
+    <div className="pacx__footer-btn">
+      <p>Request a Demo</p>
+    </div>
+
+    <div className="pacx__footer-links">
+      <div className="pacx__footer-links_logo">
+        <img src={logo2} alt="pacx_logo" />
+        <p>Pacx Lab, <br /> All Rights Reserved</p>
+      </div>
+      <div className="pacx__footer-links_div">
+        <h4>Social Media</h4>
+        <p>
+          <a href="https://twitter.com/PacksHQ" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+        </p>
+      </div>
+      <div className="pacx__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="pacx__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>cyndie@pacxlab.com</p>
+      </div>
+    </div>
+
+    <div className="pacx__footer-copyright">
+      <p>@2024 Pacx. All rights reserved.</p>
+    </div>
+  </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -12,7 +12,6 @@ const Possibility = () => {
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
         <p>With Pacx users have the freedom to explore blockchain development in an interactive manner. They also have the freedom to design and build their own learning playgrounds based on their imagination!</p>
-        <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
   )
